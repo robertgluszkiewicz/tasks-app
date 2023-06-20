edc1a882e18574ad0eda18ff2b25b124f6742c58
Robert Gluszkiewicz <robert.glu@icloud.com>
2023-06-20 23:13:44 +0200

    .gitignore file added

---
+++ src/main/java/com/robertgluszkiewicz/taskapp2/TaskApp2Application.java
@@ -0,0 +1,13 @@
+package com.robertgluszkiewicz.taskapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/robertgluszkiewicz/taskapp2/TaskApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertgluszkiewicz.taskapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
