9a3a64a3ac2f61c1997cd41692ccde489f61c478
Robert Gluszkiewicz <robert.glu@icloud.com>
2023-06-21 01:29:03 +0200

    Hiberbate validator added

--- src/main/java/com/robertgluszkiewicz/tasksapp/model/Task.java
+++ src/main/java/com/robertgluszkiewicz/tasksapp/model/Task.java
@@ -1,12 +1,12 @@
 package com.robertgluszkiewicz.tasksapp.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "task")
@@ -27,7 +27,6 @@ public class Task {
   private boolean isUrgent;
 
   public Task() {
-
   }
 
   public Task(String name, String description, boolean isUrgent) {
